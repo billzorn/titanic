b8cd44d14de869351a2c13898c47f682d8c8c291
Bill Zorn <billzorn@cs.washington.edu>
2017-08-09 22:57:55 -0700

    can describe floats in python, not so much as strings yet...

--- mpf/describefloat.py
+++ mpf/describefloat.py
@@ -16,7 +16,6 @@ def describe_format(w, p):
     emin = 1 - emax
     fmax_scale = FReal(2) - ((FReal(2) ** (1 - p)) / FReal(2))
     fmax = (FReal(2) ** emax) * fmax_scale
-    # prec = max(28, 2 ** w, p * 2) # should compute the real number
 
     return {
         'w'          : w,
@@ -26,7 +25,7 @@ def describe_format(w, p):
         'fmax_scale' : fmax_scale,
         'fmax'       : fmax,
         # 'fmax' : '(2**{})*({})'.format(str(emax), str(fmax_scale)),
-        # 'prec' : prec,
+        # precision???
     }
 
 def describe_float(S, E, T):
@@ -52,6 +51,7 @@ def describe_float(S, E, T):
 
     R = core.implicit_to_real(S, E, T)
 
+    # ordinals
     if R.isnan:
         ieee_class = 'nan'
         i = None
@@ -59,8 +59,6 @@ def describe_float(S, E, T):
         R_prev = None
         i_next = None
         R_next = None
-        lower = None
-        upper = None
     else:
         if R.isinf:
             ieee_class = 'inf'
@@ -84,8 +82,25 @@ def describe_float(S, E, T):
         if R_next.iszero:
             R_next = -R_next
 
-        lower = R_prev + ((R - R_prev) / 2)
-        upper = R + ((R_next - R) / 2)
+    # rounding
+    rounding_info = {}
+    if not R.isnan:
+        for rm in core.RNE, core.RNA, core.RTZ, core.RTP, core.RTN:
+            lower, lower_inclusive, upper, upper_inclusive = conv.implicit_to_rounding_envelope(S, E, T, rm)
+            # slow, duplicates the work of building the envelope, but meh
+            prec, lowest, midlo, midhi, highest, e = conv.shortest_dec(R, S, E, T, rm, round_correctly=False)
+            rounding_info[str(rm)] = {
+                'lower' : lower,
+                'lower_inclusive': lower_inclusive,
+                'upper' : upper,
+                'upper_inclusive': upper_inclusive,
+                'prec' : prec,
+                'lowest' : lowest,
+                'midlo' : midlo,
+                'midhi' : midhi,
+                'highest' : highest,
+                'e': e,
+            }
 
     return {
         'fmt' : fmt,
@@ -106,8 +121,7 @@ def describe_float(S, E, T):
         'R_prev' : R_prev,
         'i_next' : i_next,
         'R_next' : R_next,
-        'lower' : lower,
-        'upper' : upper,
+        'rounding_info' : rounding_info,
     }
 
 def describe_real(x, w, p):
@@ -116,29 +130,21 @@ def describe_real(x, w, p):
     assert w >= 2
     assert isinstance(p, int)
     assert p >= 2
-    #assert rm == core.RTN or rm == core.RTP or rm == core.RTZ or rm == core.RNE or rm == core.RNA
-
-    # format constants
-    umax = ((2 ** w) - 1) * (2 ** (p - 1))
-    emax = (2 ** (w - 1)) - 1
-    emin = 1 - emax
-    fmax = (FReal(2) ** emax) * (FReal(2) - ((FReal(2) ** (1 - p)) / FReal(2)))
-    prec = max(28, 2 ** w, p * 2)
 
     # After the parsing logic, we have the following variables:
     # input_repr : repr(what was passed to x)
     #          x : the input x, parsed into an int, BV, or FReal
     #          R : an FReal with the value of the raw input
-    #      below : the ordinal below R
-    #      above : the ordinal above R
+    #      i_below : the ordinal below R
+    #      i_above : the ordinal above R
 
-    # Note that the sign of 0 is missing when only considering above/below,
+    # Note that the sign of 0 is missing when only considering i_above/i_below,
     # but it can be recovered from R.
 
     # Note also that all of this information can all be determined
     # without considering a particular rounding mode.
 
-    # Finally, below and above will be None if the input is NaN.
+    # Finally, i_below and i_above will be None if the input is NaN.
 
     # Invoke some custom parsing logic on strings to figure out what they are.
     # First, generate input_repr since we might reassign here to x.
@@ -148,110 +154,121 @@ def describe_real(x, w, p):
 
     # integers are interpreted as ordinals (and never produce -0)
     if isinstance(x, int):
-        R, below, above = conv.ordinal_to_bounded_real(x, w, p)
+        R, i_below, i_above = conv.ordinal_to_bounded_real(x, w, p)
 
     # bitvectors are interpreted as packed representations
     elif isinstance(x, BV):
-        R, below, above = conv.bv_to_bounded_real(x, w, p)
+        R, i_below, i_above = conv.bv_to_bounded_real(x, w, p)
 
     # reals are just themselves
     elif isinstance(x, FReal):
-        R, below, above = conv.real_to_bounded_real(x, w, p)
+        R, i_below, i_above = conv.real_to_bounded_real(x, w, p)
 
     # what is this?
     else:
         raise ValueError('expected an int, BV, or FReal; given {}; parsed to {}'
                          .format(input_repr, repr(x)))
 
-    # What information (in strings) is useful to describe a real number?
-
-
-
-
-
-
-    # implicit bit
-    Se, Ee, C = core.implicit_to_explicit(S, E, T)
-
-    # integral values
-    s = core.uint(S)
-    e = core.uint(E) - emax
-    c = core.uint(C)
-
-    # ordinals and rounding info
-    if R.isnan:
-        i = None
-        below = None
-        above = None
+    if i_below is None or i_below == i_above:
+        # what do we do for NaN?
+        exact = True
     else:
-        i = core.implicit_to_ordinal(S, E, T)
-        below, above = core.binsearch_nearest_ordinals(r, w, p)
+        exact = False
+        Sb, Eb, Tb = core.ordinal_to_implicit(i_below, w, p)
+        R_below = core.implicit_to_real(Sb, Eb, Tb)
+        Sa, Ea, Ta = core.ordinal_to_implicit(i_above, w, p)
+        R_above = core.implicit_to_real(Sa, Ea, Ta)
 
-        if below == above:
-            exact = True
-            below = max(i - 1, -umax)
-            above = min(i + 1, umax)
-        else:
-            exact = False
+        # -0 compliant nextafter behavior
+        if R_above.iszero:
+            R_above = -R_above
 
-        Sa, Ea, Ta = core.ordinal_to_implicit(above, w, p)
-        Sb, Eb, Tb = core.ordinal_to_implicit(below, w, p)
-        guess_above = core.implicit_to_real(Sa, Ea, Ta)
-        guess_below = core.implicit_to_real(Sb, Eb, Tb)
-        difference_above = guess_above - r
-        difference_below = r - guess_below
+        difference_below = R - R_below
+        difference_above = R_above - R
 
-    # create a standard dictionary (for jsonification, etc.)
-
-    fdict = {
-        'w' : str(w),
-        'p' : str(p),
-        's' : str(s),
-        'uE' : str(E.uint),
-        'e' : str(e),
-    }
-
-    # print a bunch of stuff, may want to make this separate
-    if w == 5 and p == 11:
-        format_name = ' (binary16)'
-    elif w == 8 and p == 24:
-        format_name = ' (binary32)'
-    elif w == 11 and p == 53:
-        format_name = ' (binary64)'
-    else:
-        format_name = ''
-
-    D = conv.implicit_to_dec(S, E, T)
-
-    print('  input   : {}'.format(repr(x)))
-    print('  format  : w={}, p={}, emax={}, emin={}, umax={}, prec={}, rm={}{}'
-          .format(w, p, emax, emin, umax, prec, rm, format_name))
-    print('            the largest representable magnitude is about ~{:1.4e}'
-          .format(conv.real_to_dec(fmax, prec)))
-    print('  binary  : {} {} ({}) {}'
-          .format(str(S)[2:], str(E)[2:], str(C[p-1]), str(T)[2:]))
-    print('  approx  : ~{:1.8e}'.format(D))
-    print('  decimal : {}'.format(D))
-    print('  rational: {} /'.format(R.rational_numerator))
-    print('            {}'.format(R.rational_denominator))
-    print('  ordinal : {}'.format(i))
+        # rounding
+        rounding_info = {}
+        for rm in core.RNE, core.RNA, core.RTZ, core.RTP, core.RTN:
+            S, E, T = core.ieee_round_to_implicit(R, i_below, i_above, w, p, rm)
+            lower, lower_inclusive, upper, upper_inclusive = conv.implicit_to_rounding_envelope(S, E, T, rm)
+            # slow, duplicates the work of building the envelope, but meh
+            prec, lowest, midlo, midhi, highest, e = conv.shortest_dec(R, S, E, T, rm, round_correctly=True)
+            rounding_info[str(rm)] = {
+                'lower' : lower,
+                'lower_inclusive': lower_inclusive,
+                'upper' : upper,
+                'upper_inclusive': upper_inclusive,
+                'prec' : prec,
+                'lowest' : lowest,
+                'midlo' : midlo,
+                'midhi' : midhi,
+                'highest' : highest,
+                'e': e,
+            }
 
     if exact:
-        print('  this representation is exact.')
-        print('  next    : ~{:1.16e}, ~{:1.16e} away'
-              .format(conv.real_to_dec(guess_above, prec), conv.real_to_dec(difference_above, prec)))
-        print('  prev    : ~{:1.16e}, ~{:1.16e} away'
-              .format(conv.real_to_dec(guess_below, prec), conv.real_to_dec(difference_below, prec)))
+        return {
+            'input_repr' : input_repr,
+            'input'      : x,
+            'i_below'    : i_below,
+            'i_above'    : i_above,
+            'exact'      : exact,
+        }
     else:
-        print('  this representation is not exact.')
-        print('  above   : ~{:1.16e}, ~{:1.16e} away'
-              .format(conv.real_to_dec(guess_above, prec), conv.real_to_dec(difference_above, prec)))
-        print('  below   : ~{:1.16e}, ~{:1.16e} away'
-              .format(conv.real_to_dec(guess_below, prec), conv.real_to_dec(difference_below, prec)))
-        if i == above:
-            print('  we rounded up.')
-        else:
-            print('  we rounded down.')
+        return {
+            'input_repr' : input_repr,
+            'input'      : x,
+            'i_below'    : i_below,
+            'i_above'    : i_above,
+            'exact'      : exact,
+            'R_below' : R_below,
+            'R_above' : R_above,
+            'difference_below' : difference_below,
+            'difference_above' : difference_above,
+            'rounding_info' : rounding_info,
+        }
+
+    # # print a bunch of stuff, may want to make this separate
+    # if w == 5 and p == 11:
+    #     format_name = ' (binary16)'
+    # elif w == 8 and p == 24:
+    #     format_name = ' (binary32)'
+    # elif w == 11 and p == 53:
+    #     format_name = ' (binary64)'
+    # else:
+    #     format_name = ''
+
+    # D = conv.implicit_to_dec(S, E, T)
+
+    # print('  input   : {}'.format(repr(x)))
+    # print('  format  : w={}, p={}, emax={}, emin={}, umax={}, prec={}, rm={}{}'
+    #       .format(w, p, emax, emin, umax, prec, rm, format_name))
+    # print('            the largest representable magnitude is about ~{:1.4e}'
+    #       .format(conv.real_to_dec(fmax, prec)))
+    # print('  binary  : {} {} ({}) {}'
+    #       .format(str(S)[2:], str(E)[2:], str(C[p-1]), str(T)[2:]))
+    # print('  approx  : ~{:1.8e}'.format(D))
+    # print('  decimal : {}'.format(D))
+    # print('  rational: {} /'.format(R.rational_numerator))
+    # print('            {}'.format(R.rational_denominator))
+    # print('  ordinal : {}'.format(i))
+
+    # if exact:
+    #     print('  this representation is exact.')
+    #     print('  next    : ~{:1.16e}, ~{:1.16e} away'
+    #           .format(conv.real_to_dec(guess_above, prec), conv.real_to_dec(difference_above, prec)))
+    #     print('  prev    : ~{:1.16e}, ~{:1.16e} away'
+    #           .format(conv.real_to_dec(guess_below, prec), conv.real_to_dec(difference_below, prec)))
+    # else:
+    #     print('  this representation is not exact.')
+    #     print('  above   : ~{:1.16e}, ~{:1.16e} away'
+    #           .format(conv.real_to_dec(guess_above, prec), conv.real_to_dec(difference_above, prec)))
+    #     print('  below   : ~{:1.16e}, ~{:1.16e} away'
+    #           .format(conv.real_to_dec(guess_below, prec), conv.real_to_dec(difference_below, prec)))
+    #     if i == above:
+    #         print('  we rounded up.')
+    #     else:
+    #         print('  we rounded down.')
 
 # anecdotally, for acceptable performance we need to limit ourselves to:
 # w <= 20
@@ -267,11 +284,11 @@ if __name__ == '__main__':
                         help='exponent bits')
     parser.add_argument('-p', type=int, default=53,
                         help='significand bits')
-    parser.add_argument('-rm', choices={core.RTN, core.RTP, core.RTZ, core.RNE, core.RNA}, default=core.RNE,
-                        help='IEEE 754 rounding mode')
+    # parser.add_argument('-rm', choices={core.RTN, core.RTP, core.RTZ, core.RNE, core.RNA}, default=core.RNE,
+    #                     help='IEEE 754 rounding mode')
     parser.add_argument('x',
                         help='string to describe')
     args = parser.parse_args()
 
-    describe_float(args.x, args.w, args.p, args.rm)
+    print(describe_real(args.x, args.w, args.p))
     exit(0)
