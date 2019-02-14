c65021bfb68515a29f023a95bbb21f4b0152e3c9
Bill Zorn <billzorn@cs.washington.edu>
2019-02-13 17:41:00 -0800

    sync backend and precision settings with some javascript

--- new/titanfp/web/layout.js
+++ new/titanfp/web/layout.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 import debounce from 'lodash-es/debounce';
 
+
 export const editor_box = [];
 
 const border_px = 16;
-
 const min_width = 800;
 const min_height = 480;
 
@@ -37,5 +37,108 @@ export function resize_webtool() {
     }
 }
 
+
+function on_backend() {
+    const selected = $('#backend').val();
+    switch (selected) {
+    case 'ieee754':
+    case 'softfloat':
+        $('#float-opts').css('display', '');
+        $('#posit-opts').css('display', 'none');
+        break;
+    case 'posit':
+    case 'softposit':
+        $('#float-opts').css('display', 'none');
+        $('#posit-opts').css('display', '');
+        break;
+    }
+}
+
+function on_float_prec() {
+    const w = $('#float-w').val();
+    const p = $('#float-p').val();
+
+    if (w == 5 && p == 11) {
+        $('#float-select').val('binary16');
+    } else if (w == 8 && p == 24) {
+        $('#float-select').val('binary32');
+    } else if (w == 11 && p == 53) {
+        $('#float-select').val('binary64');
+    } else {
+        $('#float-select').val('custom');
+    }
+}
+
+function on_float_prec_select() {
+    const selected = $('#float-select').val();
+    switch (selected) {
+    case 'binary16':
+        $('#float-w').val(5);
+        $('#float-p').val(11);
+        break;
+    case 'binary32':
+        $('#float-w').val(8);
+        $('#float-p').val(24);
+        break;
+    case 'binary64':
+        $('#float-w').val(11);
+        $('#float-p').val(53);
+        break;
+    }
+}
+
+function on_posit_prec() {
+    const es = $('#posit-es').val();
+    const nbits = $('#posit-nbits').val();
+
+    if (es == 0 && nbits == 8) {
+        $('#posit-select').val('posit8');
+    } else if (es == 1 && nbits == 16) {
+        $('#posit-select').val('posit16');
+    } else if (es == 2 && nbits == 32) {
+        $('#posit-select').val('posit32');
+    } else if (es == 4 && nbits == 64) {
+        $('#posit-select').val('posit64');
+    } else {
+        $('#posit-select').val('custom');
+    }
+}
+
+function on_posit_prec_select() {
+    const selected = $('#posit-select').val();
+    switch (selected) {
+    case 'posit8':
+        $('#posit-es').val(0);
+        $('#posit-nbits').val(8);
+        break;
+    case 'posit16':
+        $('#posit-es').val(1);
+        $('#posit-nbits').val(16);
+        break;
+    case 'posit32':
+        $('#posit-es').val(2);
+        $('#posit-nbits').val(32);
+        break;
+    case 'posit64':
+        $('#posit-es').val(4);
+        $('#posit-nbits').val(64);
+        break;
+    }
+}
+
+
 resize_webtool();
 $(window).on('resize', debounce(resize_webtool, 100));
+
+on_backend();
+$('#backend').on('change', on_backend);
+
+on_float_prec();
+$('#float-w').on('change', on_float_prec);
+$('#float-p').on('change', on_float_prec);
+$('#float-select').on('change', on_float_prec_select);
+
+on_posit_prec();
+$('#posit-es').on('change', on_posit_prec);
+$('#posit-nbits').on('change', on_posit_prec);
+$('#posit-select').on('change', on_posit_prec_select);
