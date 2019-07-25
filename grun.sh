f29ebc66be0f33d2d4cb9bd11645feea06c316f8
Bill Zorn <billzorn@cs.washington.edu>
2019-07-25 15:49:14 -0700

    delete old files

--- grun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-(cd $DIR/gen_java \
-&& java -cp ".:../antlr/antlr-4.7-complete.jar" org.antlr.v4.gui.TestRig "$@")
--- www/titanic.js
+++
@@ -1,5 +0,0 @@
-const reparse = require("./reparse.js");
-exports.setupNumber = reparse.setupNumber;
-
-const fpcparser = require("./fpcparser.js");
-exports.setupCore = fpcparser.setupCore;
