fcd97ff877aca2fae07fedeeb57572e9412cc627
Bill Zorn <bill.zorn@gmail.com>
2020-03-03 23:12:14 -0800

    add build-debug command to node config; build now uses production mode

--- titanfp/web/webpack.config.js
+++ titanfp/web/webpack.config.js
@@ -5,7 +5,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: {
         layout: './layout.js',
         titanic: './titanfp.js',
