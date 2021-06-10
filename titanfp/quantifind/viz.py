0846185a897e351ea7170fd49009112393b6012c
Bill Zorn <billzorn@cs.washington.edu>
2021-06-10 02:37:28 -0700

    fix up live viz a bit

--- titanfp/quantifind/viz.py
+++ titanfp/quantifind/viz.py
@@ -2,6 +2,7 @@
 
 import os
 import math
+import re
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -16,9 +17,13 @@ class SearchData(object):
     checkpoint_name = 'latest.json'
     final_name = 'final.json'
     snapshot_name = 'frontier.json'
+    checkpoint_dirname = 'checkpoints'
+    checkpoint_re = re.compile('gen([0-9]+)' + re.escape('.json'))
+    checkpoint_key = lambda s: int(checkpoint_re.fullmatch(s).group(1))
 
     def __init__(self, search_dir):
         self.search_dir = search_dir
+        self.checkpoint_dir = os.path.join(self.search_dir, self.checkpoint_dirname)
         self.checkpoint_path = os.path.join(self.search_dir, self.checkpoint_name)
         self.final_path = os.path.join(self.search_dir, self.final_name)
         self.snapshot_path = os.path.join(self.search_dir, self.snapshot_name)
@@ -55,8 +60,25 @@ class SearchData(object):
         elif self.state is not None:
             self.frontier = self.state.frontier
 
+    def latest_checkpoint(self):
+        """get the generation number of the most recently saved full checkpoint"""
+        final = os.path.exists(self.final_path)
+        if not os.path.exists(self.checkpoint_dir):
+            return None, final
+
+        latest = None
+        for name in os.listdir(self.checkpoint_dir):
+            m = self.checkpoint_re.fullmatch(name)
+            if m:
+                cp_num = m.group(1)
+                if latest is None or cp_num > latest:
+                    latest = cp_num
+        return latest, final
 
     def refresh(self, full=False):
+        """Reread the checkpoint files from disk.
+        If full is False, only read the frontier, not the other checkpoint data.
+        """
         if full:
             self._load_files()
         else:
@@ -127,6 +149,9 @@ class LivePlot(object):
         else:
             self.refresh = refresh
         # hard-coded
+        self.new_opts = 'go'
+        self.old_opts = 'ko'
+        self.old_alpha = 0.1
         self.ghost_opts = opts.rstrip('--').rstrip('-.').rstrip('-').rstrip(':')
         self.ghost_alpha = 0.4
 
@@ -137,9 +162,13 @@ class LivePlot(object):
 
         # record keeping for interactive display
         self.known_cfgs = {}
+        self.new_line_record = None
         self.frontier_line_record = None
         self.ghost_line_record = None
         self.old_line_record = None
+
+        latest_gen, final = self.source.latest_checkpoint()
+        self.latest_gen = latest_gen
 
         # launch interaction
         if self.source.frontier:
@@ -151,6 +180,8 @@ class LivePlot(object):
 
 
     def active_lines(self):
+        if self.new_line_record is not None:
+            yield self.new_line_record
         if self.frontier_line_record is not None:
             yield self.frontier_line_record
         if self.ghost_line_record is not None:
@@ -176,19 +207,31 @@ class LivePlot(object):
 
         # clear old info
         self.ax.clear()
+        self.new_line_record = None
         self.frontier_line_record = None
         self.ghost_line_record = None
         self.old_line_record = None
 
         # set new info
         self.known_cfgs = known_cfgs
+
+        line, = self.ax.plot([], [], self.new_opts)
+        self.new_line_record = ([], [], [], line)
+
+        line, = self.ax.plot([], [], self.old_opts, alpha=self.old_alpha)
+        self.old_line_record = ([], [], [], line)
+
         xs, ys, cfgs = frontier_line_data
         line, = self.ax.plot(xs, ys, self.opts, ds='steps-post')
         self.frontier_line_record = (xs, ys, cfgs, line)
+
         if self.draw_ghosts:
             xs, ys, cfgs = ghost_line_data
             line, = self.ax.plot(xs, ys, self.ghost_opts, alpha=self.ghost_alpha)
             self.ghost_line_record = (xs, ys, cfgs, line)
+        else:
+            line, = self.ax.plot([], [], self.ghost_opts, alpha=self.ghost_alpha)
+            self.ghost_line_record = ([], [], [], line)
 
     def align(self, frontier):
         """Sort a frontier into a list of x and y coordinates, and a list of cfgs"""
@@ -251,34 +294,83 @@ class LivePlot(object):
         """Start the periodic refresh."""
 
         def update(i):
-            self.source.refresh(full=False)
-            frontier = self.source.frontier
-            if frontier is None:
-                return []
+            full_update = False
+            latest_gen, final = self.source.latest_checkpoint()
+            if ((latest_gen is not None and self.latest_gen is None)
+                or (latest_gen is not None and self.latest_gen is not None and latest_gen > self.latest_gen)):
+                self.latest_gen = latest_gen
+                full_update = True
 
-            new_cfgs = {}
+                # new gen; update everything
+                self.source.refresh(full=True)
+                cp_frontier = self.source.state.frontier
+
+                new_cfgs = {}
+                for rec in cp_frontier:
+                    cfg, qos = rec
+                    if cfg not in self.known_cfgs:
+                        new_cfgs[cfg] = rec
+
+                current_frontier, ghosts = reconstruct_frontier(cp_frontier, self.metric_fns, check=False)
+                frontier_line_data = self.align(current_frontier)
+                if self.draw_ghosts:
+                    ghost_line_data = self.align(ghosts)
+
+                # different from the state frontier, might have been partial updates
+                frontier = self.source.frontier
+
+            else:
+                # continuing previous gen in progress
+                self.source.refresh(full=False)
+                frontier = self.source.frontier
+                new_cfgs = {}
+
+            # if frontier is None:
+            #     return []
+
+            new_frontier = []
             for rec in frontier:
                 cfg, qos = rec
                 if cfg not in self.known_cfgs:
                     new_cfgs[cfg] = rec
+                    new_frontier.append(rec)
 
-            current_frontier, ghosts = reconstruct_frontier(frontier, self.metric_fns, check=False)
-            frontier_line_data = self.align(current_frontier)
-            if self.draw_ghosts:
-                ghost_line_data = self.align(ghosts)
+            new_line_data = self.align(new_frontier)
 
             # update
             updated_artists = []
             self.known_cfgs.update(new_cfgs)
-            xs, ys, cfgs, line = self.frontier_line_record
-            xs, ys, cfgs = frontier_line_data
-            self.frontier_line_record, _ = (xs, ys, cfgs, line), line.set_data(xs, ys)
+
+            xs, ys, cfgs, line = self.new_line_record
+            added_xs, added_ys, added_cfgs = new_line_data
+            xs += added_xs
+            ys += added_ys
+            cfgs += added_cfgs
+            self.new_line_record, _ = (xs, ys, cfgs, line), line.set_data(xs, ys)
             updated_artists.append(line)
-            if self.draw_ghosts:
-                 xs, ys, cfgs, line = self.ghost_line_record
-                 xs, ys, cfgs = ghost_line_data
-                 self.ghost_line_record, _ = (xs, ys, cfgs, line), line.set_data(xs, ys)
-                 updated_artists.append(line)
+
+            if full_update:
+                xs, ys, cfgs, line = self.frontier_line_record
+                xs, ys, cfgs = frontier_line_data
+                self.frontier_line_record, _ = (xs, ys, cfgs, line), line.set_data(xs, ys)
+                updated_artists.append(line)
+
+                if self.draw_ghosts:
+                     xs, ys, cfgs, line = self.ghost_line_record
+                     xs, ys, cfgs = ghost_line_data
+                     self.ghost_line_record, _ = (xs, ys, cfgs, line), line.set_data(xs, ys)
+                     updated_artists.append(line)
+
+                # copy new line to old line, and clear new line
+                new_xs, new_ys, new_cfgs, new_line = self.new_line_record
+                old_xs, old_ys, old_cfgs, old_line = self.old_line_record
+                old_xs += new_xs
+                old_ys += new_ys
+                old_cfgs += new_cfgs
+                self.old_line_record, _ = (old_xs, old_ys, old_cfgs, old_line), old_line.set_data(old_xs, old_ys)
+                self.new_line_record, _ = ([], [], [], new_line), new_line.set_data([], [])
+                updated_artists.append(old_line)
+
 
             self.ax.relim()
             self.ax.autoscale_view()
